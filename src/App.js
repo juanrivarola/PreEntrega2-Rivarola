349fe711a05522ad739b763e1ca2b0485696fb74
Juan Rivarola <juanrivarola35@gmail.com>
2023-11-01 00:21:47 -0300

    carga bien?

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css';
+import ItemListContainer from '../src/Components/ItemListContainer/ItemListContainer';
+import NavBar from '../src/Components/NavBar/NavBar';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import ItemDetailContainer from './Components/ItemDetailContainer/ItemDetailContainer';
+
+function App() {
+  return (
+    <div className="App">
+     <BrowserRouter>
+     <NavBar /> 
+        <Routes>
+        <Route path='/' element={<ItemListContainer greeting='bienvenido a shai'/>}/>
+        <Route path='/category/:id' element={<ItemListContainer/>}/>
+        <Route path='/item/:id' element={<ItemDetailContainer/>}/>
+        
+        </Routes>
+     
+     
+     </BrowserRouter>
+    
+
+    </div>
+  );
+}
+
+export default App;
